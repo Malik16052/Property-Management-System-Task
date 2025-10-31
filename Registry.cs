5229e692c05225148be15dc5b52bd1c272f590cd
Malik16052 <malikea-bpa202@code.edu.az>
2025-10-31 11:10:22 +0400

    Registry class-i

---
+++ Registry.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp7;
+
+internal class Registry
+{
+    private static int _totalBuildings=0;
+    public static int TotalBuildings { get; }
+
+
+    static Registry()
+    {
+        Console.WriteLine("Yeni bina yaradilir..");
+    }
+    public static void Register(Building b)
+    {
+        _totalBuildings++;
+        Console.WriteLine($"Registered: {b.Name} ID: {b.Id} — Total Buildings: {_totalBuildings}");
+    }
+
+
+
+}

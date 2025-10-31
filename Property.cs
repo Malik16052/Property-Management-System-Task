9ba4c6a626d10834ac5765670475709db1db7319
Malik16052 <malikea-bpa202@code.edu.az>
2025-10-31 11:05:44 +0400

    Property adli recordun ve Building class-inin yaradilmasi

---
+++ Property.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+public record Property(int Id, string Address, decimal Area, int FloorCount ,bool HasGarage);
+public abstract class Building
+{
+    int Id { get; init; }
+    string Name { get; set; }
+
+    protected Building(int id, string name)
+    {
+        Id = id;
+        Name = name;
+
+        Registry.Register(this);
+
+    }
+
+    public abstract decimal CalculateMonthlyFee();
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine($"Id:{Id} Name:{Name}");
+    }
+}
+
+public static class BuildingExtensions
+{
+    public static string ToCurrencyText(this decimal value)
+    {
+        return $"{value} AZN";
+    }
+}
